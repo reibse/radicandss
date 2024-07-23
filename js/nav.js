1bbcc3812722db3fc6726cf702af062f338744ab
reibse <138097297+reibse@users.noreply.github.com>
2024-07-24 01:20:59 +0800

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -79,3 +79,43 @@ var z = document.getElementById('btn');
       slides[slideIndex-1].style.display = "block";  
       dots[slideIndex-1].className += " active";
     }
+
+  //---------------------POP UP SCRIPT FOR FAQS------------------//
+ 
+  const faqQuestions = document.querySelectorAll('.faq-question');
+
+  faqQuestions.forEach(question => {
+    question.addEventListener('click', function() {
+      const answer = this.nextElementSibling;
+      if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+      } else {
+        // Hide all other answers
+        const allAnswers = document.querySelectorAll('.faq-answer');
+        allAnswers.forEach(ans => ans.style.display = 'none');
+        
+        // Show the clicked answer
+        answer.style.display = 'block';
+        answer.style.animation = 'slideRight 0.5s ease-out forwards';
+      }
+    });
+  });
+
+  //=======================POP UP FOR Curriculum PHOTO==========//
+  var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+    
