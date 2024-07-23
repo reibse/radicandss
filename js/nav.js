edf391fea22850aaefea6679e18f14525f5be6ca
reibse <138097297+reibse@users.noreply.github.com>
2024-07-23 17:30:24 +0800

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -37,7 +37,7 @@ var y = document.getElementById('sign-in');
 var z = document.getElementById('btn');
 
     function register(){
-        x.style.padding  = "20px 0"
+        x.style.padding  = "17px 0"
         x.style.marginTop = "100px"
         y.style.marginTop = "-320px"
         y.style.padding = "0px"
@@ -53,3 +53,29 @@ var z = document.getElementById('btn');
     }
 
 
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+    }
